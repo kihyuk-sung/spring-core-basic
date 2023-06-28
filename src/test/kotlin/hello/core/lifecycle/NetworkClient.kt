2dc79a8090d1774c1140986a9975e7c40a488183
K <kihyuk.sung@outlook.kr>
2023-06-28 20:08:14 +0900

    InitializingBean, DisposableBean
    - 초기화 / 삭제 시 사용할 메서드
    - 스프링에 의존적이게 됨

--- src/test/kotlin/hello/core/lifecycle/NetworkClient.kt
+++ src/test/kotlin/hello/core/lifecycle/NetworkClient.kt
@@ -1,13 +1,14 @@
 package hello.core.lifecycle
 
-class NetworkClient(
+import org.springframework.beans.factory.DisposableBean
+import org.springframework.beans.factory.InitializingBean
+
+class NetworkClient (
     private var url: String? = null,
-) {
+): InitializingBean, DisposableBean {
 
     init {
         println("생성자 호출, url = $url")
-        connect()
-        call("초기화 연결 메시지")
     }
 
     fun setUrl(url: String) {
@@ -19,4 +20,11 @@ class NetworkClient(
     fun call(message: String) = println("call: $url message = $message")
 
     fun disconnect() = println("close: $url")
+
+    override fun afterPropertiesSet(): Unit = this
+        .apply { connect() }
+        .call("초기화 연결 메시지")
+
+    override fun destroy() = this.disconnect()
+
 }
