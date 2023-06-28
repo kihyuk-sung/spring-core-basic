3c32cbe6aa04518396eedcc7149b09123c95c15f
K <kihyuk.sung@outlook.kr>
2023-06-28 20:51:58 +0900

    Singleton with Prototype
    - 싱글톤 객체 내에서 프로토타입을 사용하는 경우
    생성 시점에만 주입된다.
    - 싱글톤 객체에서 해당 필드 사용 시 마다 새로
    생성하고 싶은 경우, 다른 방법을 쓴다.

---
+++ src/test/kotlin/hello/core/scope/SingletonWithPrototypeTest.kt
@@ -0,0 +1,66 @@
+package hello.core.scope
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Scope
+
+class SingletonWithPrototypeTest: FunSpec({
+    test("prototypeFind") {
+        val ac = AnnotationConfigApplicationContext(PrototypeBean::class.java)
+
+        val prototypeBean1 = ac.getBean(PrototypeBean::class.java)
+        prototypeBean1.addCount()
+
+        prototypeBean1.getCount() shouldBe 1
+
+        val prototypeBean2 = ac.getBean(PrototypeBean::class.java)
+        prototypeBean2.addCount()
+
+        prototypeBean2.getCount() shouldBe 1
+    }
+
+    test("singletonClientUsePrototype") {
+        val ac = AnnotationConfigApplicationContext(PrototypeBean::class.java, ClientBean::class.java)
+
+        val clientBean1 = ac.getBean(ClientBean::class.java)
+        clientBean1.logic() shouldBe 1
+
+        val clientBean2 = ac.getBean(ClientBean::class.java)
+        clientBean2.logic() shouldBe 2
+    }
+
+}) {
+
+    @Scope("singleton")
+    class ClientBean (
+        private val prototypeBean: PrototypeBean,
+    ) {
+        fun logic(): Int {
+            prototypeBean.addCount()
+            return prototypeBean.getCount()
+        }
+
+    }
+
+    @Scope("prototype")
+    class PrototypeBean {
+        private var count = 0
+        
+        fun addCount() {
+            count += 1
+        }
+
+        fun getCount() = this.count
+
+        @PostConstruct
+        fun init() = println("PrototypeBean.init $this")
+
+
+        @PreDestroy
+        fun destroy() = println("PrototypeBean.destroy")
+    }
+
+}
