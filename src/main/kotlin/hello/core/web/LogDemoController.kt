e76c97c629a0d6ca80adf7a9edf41eae4dd80bd8
K <kihyuk.sung@outlook.kr>
2023-06-28 21:50:19 +0900

    Request scope with proxy
    - 매번 Provider를 사용하는 것은 불편함
    - Proxy를 이용하여 바로 접근하는 것 처럼 숨길 수 있음
    - 주의해서 사용

--- src/main/kotlin/hello/core/web/LogDemoController.kt
+++ src/main/kotlin/hello/core/web/LogDemoController.kt
@@ -2,7 +2,6 @@ package hello.core.web
 
 import hello.core.common.MyLogger
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody
 @Controller
 class LogDemoController(
     private val logDemoService: LogDemoService,
-    private val myLoggerProvider: ObjectProvider<MyLogger>,
+    private val myLogger: MyLogger,
 ) {
 
     @RequestMapping("/log-demo")
@@ -18,7 +17,7 @@ class LogDemoController(
     fun logDemo(request: HttpServletRequest): String = request
         .requestURL
         .toString()
-        .also { myLoggerProvider.getObject().setRequestURL(it) }
+        .also { myLogger.setRequestURL(it) }
         .let { logDemoService.logic("testId") }
         .let { "OK" }
 
--- src/main/kotlin/hello/core/web/LogDemoService.kt
+++ src/main/kotlin/hello/core/web/LogDemoService.kt
@@ -1,14 +1,12 @@
 package hello.core.web
 
 import hello.core.common.MyLogger
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Service
 
 @Service
 class LogDemoService(
-    private val myLoggerProvider: ObjectProvider<MyLogger>,
+    private val myLogger: MyLogger,
 ) {
-    fun logic(id: String) = myLoggerProvider
-        .getObject()
+    fun logic(id: String) = myLogger
         .log("service id = $id")
 }
